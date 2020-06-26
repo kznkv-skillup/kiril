7152da16e60500ef80635861439eaf8b26e167c1
kznkv-skillup <kznkv.test@gmail.com>
2020-06-26 08:22:34 +0300

    Updates

---
+++ precache-manifest.4e05e53231f70f8bb9559cf68a3faf9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29cf6ada857aa89ff5c17fea967b273",
+    "url": "/kznkv-skillup/kiril/index.html"
+  },
+  {
+    "revision": "fd451205ecbe226dc03b",
+    "url": "/kznkv-skillup/kiril/static/css/main.3916f826.chunk.css"
+  },
+  {
+    "revision": "e842c1960eb7f022f364",
+    "url": "/kznkv-skillup/kiril/static/js/2.c3c57164.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/kznkv-skillup/kiril/static/js/2.c3c57164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd451205ecbe226dc03b",
+    "url": "/kznkv-skillup/kiril/static/js/main.71c8a993.chunk.js"
+  },
+  {
+    "revision": "58812a47eb33d4acee53",
+    "url": "/kznkv-skillup/kiril/static/js/runtime-main.df6f3bf2.js"
+  }
+]);
